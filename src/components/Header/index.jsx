8303fca5ec81d81439324a392e83c2310080f354
unknown <arthur.damiani@gmail.com>
2020-12-20 23:49:23 -0300

    responsive header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import {TextField, InputAdornment, Box} from '@material-ui/core'
-import {Search, AccountCircle, PermPhoneMsg, AddShoppingCart} from '@material-ui/icons'
+import React, {useState} from 'react'
+import {TextField, InputAdornment} from '@material-ui/core'
+import {Search, AccountCircle, AddShoppingCart} from '@material-ui/icons'
+import {FaBars} from 'react-icons/fa'
 import './header.css'
 
-const theme = {
-    background: '#fff',
-}
-
 function Header() {
+    const [search, setSearch] = useState('')
+    const [sidebar, setSidebar] = useState(false)
+
+    function showSideBar() {
+        setSidebar(!sidebar)
+    }
     return (
         <header id='header'>
             <nav className='header-top'>
-                <a>Molokai</a>
-                <form>
-                    <TextField 
+                <div className="navbar">
+                    <a onClick={showSideBar} href='#' className='menu-bars'>
+                        <FaBars />
+                    </a>
+                </div>
+                <a href='#' className='header-logo'>Molokai</a>
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    console.log(search)
+                }}>
+                    <TextField
+                        value={search}
+                        onChange={(e) => {setSearch(e.target.value)}}
                         InputProps={{endAdornment: (<InputAdornment position='end'><Search color='primary' className='logo-input' /></InputAdornment>)}}
                         placeholder='Digite o que você procura' 
                         variant='outlined'
@@ -23,30 +36,36 @@ function Header() {
                     />
                 </form>
                 <nav className='nav-container'>
-                    <a className='nav-item'>
+                    <a href='#' className='nav-item'>
                         <AccountCircle fontSize="large" />
                         <p className='nav-item-legenda'>Entre ou cadastre-se</p>
                     </a>
-                    <a className='nav-item'>
-                        <PermPhoneMsg fontSize="large" />
-                        <p className='nav-item-legenda'>Central de ajuda</p>
-                    </a>
-                    <a className='nav-item'>
+                    <a href='#' className='nav-item'>
                         <AddShoppingCart fontSize="large" />
-                        <p className='nav-item-legenda'>Carrinho de compras</p>
+                        <p className='nav-item-legenda carrinho-legenda'>Carrinho de compras</p>
                     </a>
                 </nav>
             </nav>
-            <nav className='menu-container'>
-                <a className='menu-item'>Surf</a>
-                <a className='menu-item'>Masculino</a>
-                <a className='menu-item'>Feminino</a>
-                <a className='menu-item'>Calçados</a>
-                <a className='menu-item'>Juvenil</a>
-                <a className='menu-item'>Óculos</a>
-                <a className='menu-item'>Relógios</a>
-                <a className='menu-item'>Acessórios</a>
-                <a className='menu-item'>Marcas</a>
+            <nav onClick={showSideBar} className={sidebar ? 'menu-container active' : 'menu-container'}>
+                <div className='sidebar-top'>
+                    <a href='#' className='sidebar-item'>
+                        <AccountCircle fontSize="large" />
+                        <p className='nav-item-legenda'>Entre ou cadastre-se</p>
+                    </a>
+                    <a href='#' className='sidebar-item'>
+                        <AddShoppingCart fontSize="large" />
+                        <p className='nav-item-legenda'>Carrinho de compras</p>
+                    </a>
+                </div>
+                <a href='#' className='menu-item'>Surf</a>
+                <a href='#' className='menu-item'>Masculino</a>
+                <a href='#' className='menu-item'>Feminno</a>
+                <a href='#' className='menu-item'>Calçados</a>
+                <a href='#' className='menu-item'>Juvenil</a>
+                <a href='#' className='menu-item'>Óculos</a>
+                <a href='#' className='menu-item'>Relógios</a>
+                <a href='#' className='menu-item'>Acessórios</a>
+                <a href='#' className='menu-item'>Marcas</a>
             </nav>
         </header>
     )
