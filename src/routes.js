58d2cb805c1e707e5a831cd2ba0489e441558483
matheus <matheusbvieira@hotmail.com>
2021-01-12 12:47:41 -0300

    add components errors

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import ProductsCard from './pages/ProductsCard';
 import DetailsProduct from './components/DetailsProduct';
 import Header from './components/Header'
 import FloatCart from './components/FloatCart';
-
+import InternalServer from '../components/ErrorPages/InternalServer/InternalServer';
 
 function Routes() {
 	return (
@@ -14,6 +14,7 @@ function Routes() {
 			<Route path="/" exact component={Landing} />
 			<Route path="/products" exact component={ProductsCard} />
 			<Route path="/detailsProducts/:id" exact component={DetailsProduct} />
+			<Route path="/500" component={InternalServer} />
 		</BrowserRouter>
 	)
 }
