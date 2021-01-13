725da86ddd84f504c62e5da051d2ea0847d9c5f5
unknown <arthur.damiani@gmail.com>
2021-01-13 11:49:12 -0300

    footer responsive

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,21 +6,21 @@ import './footer.css'
 function Footer() {
     return (
         <footer id='footer'>
-            <div className="footer-right-side">
-                <a className='logo-footer' href='#'>Molokai</a>
-                <p className='email-footer'>molokai@gmail.com</p>
-                <div className="address-footer">
-                    <p>Praia mole, 272</p>
-                </div>
-            </div>
-            <div className="footer-left-side">
-                <ul className="social-media">
-                    <li className="social-media-item"><a href='#'><Instagram fontSize="large" /></a></li>
-                    <li className="social-media-item middle-item"><a href='#'><Facebook fontSize="large" /></a></li>
-                    <li className="social-media-item"><a href='#'><Twitter fontSize="large" /></a></li>
-                </ul>
-                <p>&copy; 2020 Molokai.</p>
-            </div>
+            <ul className="social-media">
+                <li className="social-media-item"><a href='#'><Facebook fontSize="large" /></a></li>
+                <li className="social-media-item middle-item"><a href='#'><Instagram fontSize="large" /></a></li>
+                <li className="social-media-item"><a href='#'><Twitter fontSize="large" /></a></li>
+            </ul>
+            <ul className='footer-services'>
+                <li className='no-decoration'>Info</li>
+                <li>Suporte</li>
+                <li>Acompanhe seu pedido</li>
+            </ul>
+            <ul className='footer-services'>
+                <li className='no-decoration'>Formas de pagamento</li>
+                <li>Politicas de privacidade</li>
+            </ul>
+            <p className='copyright'>&copy; Copyright 2021 - Todos os direitos reservados</p>
         </footer>
     )
 }
